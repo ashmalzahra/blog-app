7c663152c8092b1d47483629d8032878411aa92c
ashmalzahra <ashmal.zahraa@gmail.com>
2023-05-05 21:25:07 +0500

    Add unit specs for user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,9 @@ class Post < ApplicationRecord
   after_save :update_post_counter
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0  }
-  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0  }
-  
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def last_five_comments
     comments.order('created_at DESC').limit(5)
   end
