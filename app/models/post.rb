64d03741670dd8f60493d0d6b2a086837487fa29
ashmalzahra <ashmal.zahraa@gmail.com>
2023-05-19 21:11:40 +0500

    Instal tailwindcss and refactor code

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,20 @@
 class Post < ApplicationRecord
-  has_many :likes, foreign_key: 'post_id'
-  has_many :comments, foreign_key: 'post_id'
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  after_save :update_post_counter
-
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
-  def last_five_comments
-    comments.order('created_at DESC').limit(5)
-  end
+  after_save :update_post_counter
 
   private
 
   def update_post_counter
     author.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "users#index"
-  get "/users", to: "users#index" 
-  get "/users/:id", to: "users#show"
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/users/:user_id/posts/:id", to: "posts#show"
-  get "posts/new", to: "posts#new"
-  post "posts/create", to: "posts#create"
-  post "/users/:user_id/posts/:id/add_comment", to: "comments#add_comment"
-  post "/users/:user_id/posts/:id/add_like", to: "likes#add_like"
+      post :auth, to: 'authentication#create'
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
+  
+  resources :users, only: [:index, :show] do
+    resources :posts, except: [:update, :edit] do
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:create]
+    end
+  end
+
+  root 'users#index'
 end
